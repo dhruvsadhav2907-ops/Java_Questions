6ea679aa26eb1676b5a11ca174f7a6f194316e1a
dhruvsadhav2907-ops <dhruv.sadhav.btech2025@sitpune.edu.in>
2026-08-21 21:45:42 +0530

    Create a Java program by developing a user-defined package named library that contains a class to store and display book details such as book ID, title, author, and price. Import the package into the main program and display the book information.

---
+++ LibraryManagement.java
@@ -0,0 +1,9 @@
+import LibraryPackage.Library;
+
+
+class LibraryManagement{
+	public static void main(String[] args){
+		Library l = new Library(101,"Harry Potter","J.K.Rowling",2500);
+		l.displayDetails();
+	}
+}
