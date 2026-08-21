99a41dded2d85abd02f41d9e6349febcfc7a6412
dhruvsadhav2907-ops <dhruv.sadhav.btech2025@sitpune.edu.in>
2026-08-21 21:42:13 +0530

    Write a Java program to create and use user-defined packages.

---
+++ Square.java
@@ -0,0 +1,20 @@
+package Mysqpackage;
+
+public class Square{
+	
+	public int displayIntArea(int a){
+		return a*a;
+	}
+
+	public float displayFloatArea(float a){
+		return a*a;
+	}
+	
+	public double displayDoubleArea(double a){
+		return a*a;
+	}
+	public static void main(String[] args){
+		System.out.println("Package Called");
+	}
+ 
+}
